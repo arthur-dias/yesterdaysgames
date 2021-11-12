88fc664662cba4e95f6448e6ec7fcc48dd241a2d
Arthur Dias <dias.arthurr@gmail.com>
2021-11-12 08:32:11 -0300

    Removed about page and Navbar menu

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,10 +5,6 @@ const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <h1 className={styles.title}>Yesterday&apos;s Matches</h1>
-      <div className={styles.links}>
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
-      </div>
     </div>
   )
 }
