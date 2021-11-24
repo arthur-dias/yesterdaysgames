059f43d5a9c1a9ce93fee47430e6fc5140175b03
Arthur Dias <dias.arthurr@gmail.com>
2021-11-24 15:09:09 -0300

    Changed footer style

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,8 @@ const Header = () => {
     <div className={styles.header}>
       <p className={styles.text}>
         Slept through the game? Dinner took longer tha expected? Your data
-        runned out during the party?
+        runned out during the party? All of it?
       </p>
-      <p className={styles.text}>All of the above?</p>
       <p className={styles.text}>
         Simply select one league on the list and get access to the post match
         thread on Reddit and the top 5 most viewed highlights and discussion
