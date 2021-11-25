a2b343733c42883fb8c6901e254c4305c5449697
Arthur Dias <dias.arthurr@gmail.com>
2021-11-25 12:12:52 -0300

    Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,16 @@
+import Content from '../components/Content'
+import moduleName from '../components/Content'
+
+const Custom404 = () => {
+  return (
+    <div>
+      <p style={{ textAlign: 'center', color: '#fffffe' }}>
+        The page you were looking for doesn't appear to exist. Try selecting one
+        of the leagues below.
+      </p>
+      <Content />
+    </div>
+  )
+}
+
+export default Custom404
